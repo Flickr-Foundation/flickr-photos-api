1b5bc2a775cd6c811f5b7b7ced16aafb8c0ffc7a
Alex Chan <alex@alexwlchan.net>
2023-11-05 16:05:23 +0000

    Release version v1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+## v1.0.0 - 2023-11-05
+
+Initial public release.
--- src/flickr_photos_api/__init__.py
+++ src/flickr_photos_api/__init__.py
@@ -1,7 +1,7 @@
 from .api import FlickrPhotosApi
 
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 
 
 __all__ = ["FlickrPhotosApi", "__version__"]
