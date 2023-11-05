b47e91c359c880408b71af08673b728c72e9ccc2
Alex Chan <alex@alexwlchan.net>
2023-11-05 17:23:01 +0000

    Export the exceptions and types as part of the top-level module

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## v1.0.2 - 2023-11-05
+
+Export the exceptions and types as part of the top-level module.
+
 ## v1.0.1 - 2023-11-05
 
 Fix a bug with the type definitions on Python 3.7.
--- src/flickr_photos_api/__init__.py
+++ src/flickr_photos_api/__init__.py
@@ -1,7 +1,34 @@
 from .api import FlickrPhotosApi
+from .exceptions import FlickrApiException, ResourceNotFound, LicenseNotFound
+from ._types import (
+    License,
+    User,
+    DateTaken,
+    Size,
+    SinglePhoto,
+    CollectionOfPhotos,
+    PhotosInAlbum,
+    PhotosInGallery,
+    PhotosInGroup,
+)
 
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 
-__all__ = ["FlickrPhotosApi", "__version__"]
+__all__ = [
+    "FlickrPhotosApi",
+    "FlickrApiException",
+    "ResourceNotFound",
+    "LicenseNotFound",
+    "License",
+    "User",
+    "DateTaken",
+    "Size",
+    "SinglePhoto",
+    "CollectionOfPhotos",
+    "PhotosInAlbum",
+    "PhotosInGallery",
+    "PhotosInGroup",
+    "__version__",
+]
