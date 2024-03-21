13a0df516063785011f61ac48733b6e928f5a871
Alex Chan <alex@alexwlchan.net>
2024-03-21 11:54:16 +0000

    Remove an unused type

--- src/flickr_photos_api/types.py
+++ src/flickr_photos_api/types.py
@@ -34,13 +34,6 @@ class DateTaken(TypedDict):
     granularity: TakenGranularity
 
 
-class NewSize(TypedDict):
-    width: int
-    height: int
-    media: str
-    source: str
-
-
 class Size(TypedDict):
     label: str
     width: int
