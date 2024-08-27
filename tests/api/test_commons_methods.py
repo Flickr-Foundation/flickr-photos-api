417010c03c141e14095e4a587d30c4c0fe293390
Alex Chan <alex@alexwlchan.net>
2024-08-27 12:05:47 +0100

    Add docstrings for `test_commons_methods.py`

--- tests/api/test_commons_methods.py
+++ tests/api/test_commons_methods.py
@@ -1,19 +1,26 @@
+"""
+Tests for ``flickr_photos_api.api.commons_methods``.
+"""
+
 import datetime
 
 from flickr_photos_api import FlickrApi
 
 
-class TestCommonsMethods:
-    def test_list_commons_institutions(self, api: FlickrApi) -> None:
-        institutions = api.list_commons_institutions()
+def test_list_commons_institutions(api: FlickrApi) -> None:
+    """
+    We get a list of all the Commons Institutions with
+    ``list_commons_institutions``.
+    """
+    institutions = api.list_commons_institutions()
 
-        assert len(institutions) == 113
-        assert institutions[0] == {
-            "date_launch": datetime.datetime(
-                2024, 1, 29, 17, 24, 17, tzinfo=datetime.timezone.utc
-            ),
-            "license_url": "https://flickr.org",
-            "name": "CommonsTestAccount",
-            "site_url": "https://flickr.org",
-            "user_id": "200049760@N08",
-        }
+    assert len(institutions) == 113
+    assert institutions[0] == {
+        "date_launch": datetime.datetime(
+            2024, 1, 29, 17, 24, 17, tzinfo=datetime.timezone.utc
+        ),
+        "license_url": "https://flickr.org",
+        "name": "CommonsTestAccount",
+        "site_url": "https://flickr.org",
+        "user_id": "200049760@N08",
+    }
