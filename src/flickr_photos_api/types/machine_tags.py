e8c590ca8b9f14c8d0e68a8873cd9b95b46dadfa
Alex Chan <alex@alexwlchan.net>
2024-09-09 09:34:25 +0100

    Add tests for `machine_tags.py`

--- src/flickr_photos_api/types/machine_tags.py
+++ src/flickr_photos_api/types/machine_tags.py
@@ -1,3 +1,10 @@
+"""
+On Flickr, machine tag are used to store structured data in types,
+e.g. ``geo:lat=12.34`` is a tag with location information.
+
+This file has a few functions for dealing with machine tags.
+"""
+
 import collections
 import re
 import typing
@@ -33,6 +40,20 @@ def get_machine_tags(tags: list[str]) -> MachineTags:
     """
     Given a list of raw tags on Flickr, parse the machine tags
     as key-value pairs.
+
+    The keys are namespace/predicate; the values are values.
+
+    Example:
+
+        >>> tags = [
+        ...     "square",
+        ...     "shape:sides=4",
+        ...     "shape:color=red",
+        ...     "shape:color=blue"
+        ... ]
+        ...
+        >>> get_machine_tags(tags)
+        {"shape:sides": ["4"], "shape:color": ["red", "blue"]}
 
     This function is a "best effort" parsing of machine tags -- it may
     not match Flickr perfectly, but is meant to make it easier for
--- tests/types/test_machine_tags.py
+++ tests/types/test_machine_tags.py
@@ -1,39 +1,58 @@
-import pytest
+"""
+Tests for ``flickr_photos_api.types.machine_tags``.
+"""
 
-from flickr_photos_api.types import get_machine_tags, MachineTags
+from flickr_photos_api.types import get_machine_tags
 
 
-@pytest.mark.parametrize(
-    ["tags", "machine_tags"],
-    [
-        ([], {}),
-        (["Natural history", "Periodicals", "Physical sciences"], {}),
-        (
-            [
-                "Natural history",
-                "Periodicals",
-                "Physical sciences",
-                "bhl:page=33665645",
-            ],
-            {"bhl:page": ["33665645"]},
-        ),
-        (
-            [
-                "Natural history",
-                "Periodicals",
-                "Physical sciences",
-                "bhl:page=33665645",
-                "taxonomy:genus=Loxodonta",
-                "taxonomy:binomial=Elephas maximus",
-                "taxonomy:binomial=Elephas indicus",
-            ],
-            {
-                "bhl:page": ["33665645"],
-                "taxonomy:genus": ["Loxodonta"],
-                "taxonomy:binomial": ["Elephas maximus", "Elephas indicus"],
-            },
-        ),
-    ],
-)
-def test_get_machine_tags(tags: list[str], machine_tags: MachineTags) -> None:
+def test_empty_tags_is_empty_machine_tags() -> None:
+    """
+    If there are no tags, there are no machine tags.
+    """
+    assert get_machine_tags(tags=[]) == {}
+
+
+def test_keyword_tags_are_not_machine_tags() -> None:
+    """
+    If all the tags are "keywords" (unstructured text) then there
+    are no machine tags.
+    """
+    tags = ["Natural history", "Periodicals", "Physical sciences"]
+    assert get_machine_tags(tags) == {}
+
+
+def test_get_single_machine_tag() -> None:
+    """
+    If some tags are machine tags and some aren't, only the machine tags
+    are returned.
+    """
+    tags = [
+        "Natural history",
+        "Periodicals",
+        "Physical sciences",
+        "bhl:page=33665645",
+    ]
+    assert get_machine_tags(tags) == {"bhl:page": ["33665645"]}
+
+
+def test_get_repeated_namespace_predicate() -> None:
+    """
+    If there are multiple machine tags with the same namespace/predicate,
+    they're grouped together.
+    """
+    tags = [
+        "Natural history",
+        "Periodicals",
+        "Physical sciences",
+        "bhl:page=33665645",
+        "taxonomy:genus=Loxodonta",
+        "taxonomy:binomial=Elephas maximus",
+        "taxonomy:binomial=Elephas indicus",
+    ]
+    machine_tags = {
+        "bhl:page": ["33665645"],
+        "taxonomy:genus": ["Loxodonta"],
+        "taxonomy:binomial": ["Elephas maximus", "Elephas indicus"],
+    }
+
     assert get_machine_tags(tags) == machine_tags
