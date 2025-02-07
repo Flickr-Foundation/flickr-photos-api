2260d4b5922faf24bcdebccfb010fea2092580dd
Alex Chan <alex@alexwlchan.net>
2025-02-07 15:19:19 +0000

    Fix a couple of issues with the newest dependencies

--- tests/utils.py
+++ tests/utils.py
@@ -5,10 +5,10 @@ These functions are *not* part of the published library, and are
 just for use in the flickr-photos-api tests.
 """
 
-import os
+from pathlib import Path
 import typing
 
-from nitrate.json import DatetimeDecoder
+from nitrate.json import NitrateDecoder
 from nitrate.types import read_typed_json
 
 T = typing.TypeVar("T")
@@ -18,8 +18,10 @@ def get_fixture(filename: str, *, model: type[T]) -> T:
     """
     Read a JSON fixture and check it's the right type.
     """
+    fixtures_dir = Path("tests/fixtures/api_responses")
+
     return read_typed_json(
-        os.path.join("tests/fixtures/api_responses", filename),
+        fixtures_dir / filename,
         model=model,
-        cls=DatetimeDecoder,
+        cls=NitrateDecoder,
     )
