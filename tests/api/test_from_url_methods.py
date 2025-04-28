a837c6d7aaa5be3c7651ebcc7952542e3ba1d96a
Alex Chan <alex@alexwlchan.net>
2025-04-28 16:07:45 +0100

    Remove the `get_photos_from_flickr_url` method

--- tests/api/test_from_url_methods.py
+++ tests/api/test_from_url_methods.py
@@ -2,6 +2,7 @@
 Tests for ``flickr_photos_api.api.from_url_methods``.
 """
 
+from flickr_url_parser import ParseResult
 import pytest
 
 from flickr_photos_api import FlickrApi
@@ -17,30 +18,53 @@ from utils import get_fixture
 
 class TestGetPhotosFromFlickrUrl:
     """
-    Tests for ``FromUrlMethods.get_photos_from_flickr_url``.
+    Tests for ``FromUrlMethods.get_photos_from_parsed_flickr_url``.
     """
 
     def test_get_single_photo_url(self, api: FlickrApi) -> None:
         """
         Get the URL for a single photo.
         """
-        actual = api.get_photos_from_flickr_url(
-            url="https://www.flickr.com/photos/coast_guard/32812033543"
+        actual = api.get_photos_from_parsed_flickr_url(
+            parsed_url={
+                "type": "single_photo",
+                "photo_id": "32812033543",
+                "user_url": "https://www.flickr.com/photos/coast_guard/",
+                "user_id": None,
+            }
         )
         expected = get_fixture("32812033543.json", model=SinglePhoto)
 
         assert actual == expected
 
-    @pytest.mark.parametrize("path_identifier", ["joshuatreenp", "115357548@N08"])
-    def test_get_album_url(self, api: FlickrApi, path_identifier: str) -> None:
+    def test_get_album_url_with_path_alias(self, api: FlickrApi) -> None:
         """
         Get photos from a URL to an album.
         """
-        url = (
-            f"https://www.flickr.com/photos/{path_identifier}/albums/72157640898611483"
+        actual = api.get_photos_from_parsed_flickr_url(
+            parsed_url={
+                "type": "album",
+                "user_url": "https://www.flickr.com/photos/joshuatreenp/",
+                "album_id": "72157640898611483",
+                "page": 1,
+            }
         )
+        expected = get_fixture("album-72157640898611483.json", model=PhotosInAlbum)
 
-        actual = api.get_photos_from_flickr_url(url)
+        assert actual == expected
+
+    def test_get_album_url_with_user_id(self, api: FlickrApi) -> None:
+        """
+        Get photos from a URL to an album.
+        """
+        actual = api.get_photos_from_parsed_flickr_url(
+            parsed_url={
+                "type": "album",
+                "user_url": "https://www.flickr.com/photos/115357548@N08/",
+                "album_id": "72157640898611483",
+                "page": 1,
+            }
+        )
         expected = get_fixture("album-72157640898611483.json", model=PhotosInAlbum)
 
         assert actual == expected
@@ -49,8 +73,13 @@ class TestGetPhotosFromFlickrUrl:
         """
         Get photos from a URL that points to the second page of an album.
         """
-        actual = api.get_photos_from_flickr_url(
-            url="https://www.flickr.com/photos/joshuatreenp/albums/72157640898611483/page2"
+        actual = api.get_photos_from_parsed_flickr_url(
+            parsed_url={
+                "type": "album",
+                "user_url": "https://www.flickr.com/photos/joshuatreenp/",
+                "album_id": "72157640898611483",
+                "page": 2,
+            }
         )
         expected = get_fixture(
             "album-72157640898611483-page2.json", model=PhotosInAlbum
@@ -58,16 +87,34 @@ class TestGetPhotosFromFlickrUrl:
 
         assert actual == expected
 
-    @pytest.mark.parametrize("path_identifier", ["spike_yun", "132051449@N06"])
-    def test_get_photos_in_user_profile(
-        self, api: FlickrApi, path_identifier: str
-    ) -> None:
+    def test_get_photos_in_user_profile_with_path_alias(self, api: FlickrApi) -> None:
         """
         Get photos from a URL that points to a user's profile.
         """
-        url = f"https://www.flickr.com/photos/{path_identifier}/"
+        actual = api.get_photos_from_parsed_flickr_url(
+            {
+                "type": "user",
+                "user_url": "https://www.flickr.com/photos/spike_yun/",
+                "user_id": None,
+                "page": 1,
+            }
+        )
+        expected = get_fixture("user-spike_yun.json", model=CollectionOfPhotos)
 
-        actual = api.get_photos_from_flickr_url(url)
+        assert actual == expected
+
+    def test_get_photos_in_user_profile_with_user_id(self, api: FlickrApi) -> None:
+        """
+        Get photos from a URL that points to a user's profile.
+        """
+        actual = api.get_photos_from_parsed_flickr_url(
+            {
+                "type": "user",
+                "user_url": "https://www.flickr.com/photos/132051449@N06/",
+                "user_id": "132051449@N06",
+                "page": 1,
+            }
+        )
         expected = get_fixture("user-spike_yun.json", model=CollectionOfPhotos)
 
         assert actual == expected
@@ -76,8 +123,8 @@ class TestGetPhotosFromFlickrUrl:
         """
         Get photos from a URL that points to a gallery.
         """
-        actual = api.get_photos_from_flickr_url(
-            url="https://www.flickr.com/photos/meldaniel/galleries/72157716953066942/"
+        actual = api.get_photos_from_parsed_flickr_url(
+            parsed_url={"type": "gallery", "gallery_id": "72157716953066942", "page": 1}
         )
         expected = get_fixture("gallery-72157716953066942.json", model=PhotosInGallery)
 
@@ -87,8 +134,12 @@ class TestGetPhotosFromFlickrUrl:
         """
         Get photos from a URL that points to a group.
         """
-        actual = api.get_photos_from_flickr_url(
-            url="https://www.flickr.com/groups/geologists/"
+        actual = api.get_photos_from_parsed_flickr_url(
+            {
+                "type": "group",
+                "group_url": "https://www.flickr.com/groups/geologists",
+                "page": 1,
+            }
         )
         expected = get_fixture("group-geologists.json", model=PhotosInGroup)
 
@@ -98,8 +149,8 @@ class TestGetPhotosFromFlickrUrl:
         """
         Get photos from a URL that points to a tag.
         """
-        actual = api.get_photos_from_flickr_url(
-            url="https://www.flickr.com/photos/tags/botany"
+        actual = api.get_photos_from_parsed_flickr_url(
+            parsed_url={"type": "tag", "tag": "botany", "page": 1}
         )
         expected = get_fixture("tag-botany.json", model=CollectionOfPhotos)
 
@@ -107,28 +158,52 @@ class TestGetPhotosFromFlickrUrl:
 
 
 @pytest.mark.parametrize(
-    "url",
+    "parsed_url",
     [
         pytest.param(
-            "https://www.flickr.com/photos/joshuatreenp/albums/72157640898611483",
+            {
+                "type": "album",
+                "user_url": "https://www.flickr.com/photos/joshuatreenp/",
+                "album_id": "72157640898611483",
+                "page": 1,
+            },
             id="album",
         ),
-        pytest.param("https://www.flickr.com/photos/spike_yun", id="user"),
         pytest.param(
-            "https://www.flickr.com/photos/meldaniel/galleries/72157716953066942",
+            {
+                "type": "user",
+                "user_url": "https://www.flickr.com/photos/spike_yun/",
+                "user_id": None,
+                "page": 1,
+            },
+            id="user",
+        ),
+        pytest.param(
+            {"type": "gallery", "gallery_id": "72157716953066942", "page": 1},
             id="gallery",
         ),
-        pytest.param("https://www.flickr.com/groups/geologists/pool", id="group"),
-        pytest.param("https://www.flickr.com/photos/tags/botany", id="tag"),
+        pytest.param(
+            {
+                "type": "group",
+                "group_url": "https://www.flickr.com/groups/geologists",
+                "page": 1,
+            },
+            id="group",
+        ),
+        pytest.param({"type": "tag", "tag": "botany", "page": 1}, id="tag"),
     ],
 )
-def test_get_photos_from_flickr_url_is_paginated(api: FlickrApi, url: str) -> None:
+def test_get_photos_from_flickr_url_is_paginated(
+    api: FlickrApi, parsed_url: ParseResult
+) -> None:
     """
     If you look up photos from a paginated URL, you get photos from
     that specific page.
     """
-    first_resp = api.get_photos_from_flickr_url(url)
-    second_resp = api.get_photos_from_flickr_url(url + "/page2")
+    first_resp = api.get_photos_from_parsed_flickr_url(parsed_url)
+
+    parsed_url["page"] = 2  # type: ignore
+    second_resp = api.get_photos_from_parsed_flickr_url(parsed_url)
 
     assert first_resp["photos"] != second_resp["photos"]  # type: ignore
 
